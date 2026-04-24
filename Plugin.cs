924d974b406d10a6a2be3eb401a70c48ca244b3d
captnced <cedric.elsaesser1603@gmail.com>
2026-04-24 17:22:06 +0200

    Made compatible with IMHelper 3.3.0

--- Plugin.cs
+++ Plugin.cs
@@ -10,24 +10,21 @@ namespace ConsoleCommands;
 
 [BepInPlugin(Guid, Name, Version)]
 [BepInProcess("IXION.exe")]
-[BepInDependency("captnced.IMHelper", ">=3.2.0")]
+[BepInDependency("captnced.IMHelper", ">=3.3.0")]
 public class Plugin : BasePlugin
 {
     private const string Guid = "captnced.ConsoleCommands";
     private const string Name = "ConsoleCommands";
-    private const string Version = "1.0.1";
+    private const string Version = "1.0.2";
     internal new static ManualLogSource Log;
     internal static bool enabled = true;
-
+    private static SettingsHelper.SettingsSection sec;
     public override void Load()
     {
         Log = base.Log;
         if (IL2CPPChainloader.Instance.Plugins.ContainsKey("captnced.IMHelper")) setEnabled();
         GameStateHelper.addSceneChangedToInGameListener(Console.inGameListener);
         GameStateHelper.addSceneChangedListener(Console.mainMenuListener, GameStateHelper.GameScene.MainMenu);
-        _ = new SettingsHelper.KeySetting(new SettingsHelper.SettingsSection("Console Commands"), "Open Console",
-            "Opens the mods console. Feedback is only provided in the BepInEx Console", KeyCode.F8,
-            Console.consoleHotkeyPressed, true);
         KeyListenerHelper.addInGameKeyListener(KeyCode.Escape, Console.escPressed);
         if (!enabled)
             Log.LogInfo("Disabled by IMHelper!");
@@ -56,11 +53,17 @@ public class Plugin : BasePlugin
 
     private static void init()
     {
+        sec = new SettingsHelper.SettingsSection("Console Commands");
+        SettingsHelper.addTopSection(sec);
+        sec.addItem(new SettingsHelper.KeySetting("Open Console",
+            "Opens the mods console. Feedback is only provided in the BepInEx Console", KeyCode.F8,
+            Console.consoleHotkeyPressed, true));
         Log.LogInfo("Loaded \"" + Name + "\" version " + Version + "!");
     }
 
     private static void disable()
     {
+        sec.destroy();
         Log.LogInfo("Unloaded \"" + Name + "\" version " + Version + "!");
     }
 
